b394726c6ad6d19bf72f0b6362157faa1b72e6a4
phlaster <125278254+phlaster@users.noreply.github.com>
2026-07-22 16:23:34 +0300

    deps update dev url QR-code

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {
   defineConfig
 } from 'vite';
+import ServerUrlCopy from 'vite-plugin-url-copy';
 
 export default defineConfig({
   base: './',
@@ -11,5 +12,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173
-  }
+  },
+  plugins: [
+    ServerUrlCopy({
+      qrcode: {
+        disabled: false,
+      },
+    })
+  ],
 });
